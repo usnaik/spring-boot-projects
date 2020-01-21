a6f9cd8a163b0a0a3e256871759e5440a50a9a0b
usnaik <upendra_naik@yahoo.com>
2020-01-21 20:03:21 +0530

    cleanup

--- spring-security-jwt/src/main/java/com/upen/springsecurityjwtjpa/HelloWorldController.java
+++ spring-security-jwt/src/main/java/com/upen/springsecurityjwtjpa/HelloWorldController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.upen.springsecurityjwt.util.JwtUtil;
+import com.upen.springsecurityjwtjpa.util.JwtUtil;
 import com.upen.springsecurityjwtjpa.models.AuthenticationRequest;
 import com.upen.springsecurityjwtjpa.models.AuthenticationResponse;
 
