892163429984e4e6df261130734fb25ed6443782
usnaik <upendra_naik@yahoo.com>
2020-01-22 12:10:58 +0530

    test

---
+++ spring-security-oauth/src/test/java/com/upen/springboot/SpringSecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upen.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
